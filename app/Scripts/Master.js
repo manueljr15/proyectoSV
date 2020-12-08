ee2d1d2d4f166e44e6d624cc21d46f4b8805b093
Oswaldo Juarez <OJuarez@prodamex.local>
2020-12-08 07:27:09 -0600

    Proyecto sistemas Virtuales

---
+++ app/Scripts/Master.js
@@ -0,0 +1,10 @@
+﻿function asignarAlumnos(id_docente) {
+    $.ajax({
+        url: '/Tutores_Alumnos/Create?id=' + id_docente,
+        async: true,
+        success: function (data) {
+            document.getElementById("bodyModal").innerHTML = data;
+        }
+    });
+}
+
